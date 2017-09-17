1d6de2f839b9fb96f0510cfb29c9e1e19b7eb6a9
djangorris <lukkyjay@gmail.com>
2017-09-16 20:15:00 -0600

    just need to remove columns and rows from .xlsm file and this will output a plot for Rate, AV, and Member_Months

--- clean.py
+++ clean.py
@@ -4,6 +4,12 @@ import matplotlib.colors as colors
 import matplotlib.patches as mpatches
 from matplotlib import style
 import numpy as np
+###
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+import matplotlib.patches as mpatches
+from matplotlib import style
+import numpy as np
 
 ### FIRST, REMOVE EXTRA ROWS AND COLUMNS IN NUMBERS AND EXPORT TO CSV ###
 ### Should have rows in order ['Plan Name', Metal', 'AV Metal Value', 'Category', 'Plan Type', 'Exchange Plan?', Plan_Adjusted_Index_Rate', 'Member_Months']]
@@ -27,25 +33,15 @@ df.Member_Months = df.Member_Months.str.replace(',', '').astype(int).fillna(0)
 # separate ON and OFF exchange
 df_on = df[df.Exchange == 'Yes']
 df_off = df[df.Exchange == 'No']
-# remove "Exchange" column
-del df_on['Exchange']
-del df_off['Exchange']
 # round AV column to 3 decimal places
 df_on.AV = df.AV.round(3)
 df_off.AV = df.AV.round(3)
 # groupby Plan_Name and sum member months
-df_on_grouped = df_on.groupby('Plan_Name', as_index=False)[['Metal', 'AV', 'Category', 'Network', 'Plan_Adjusted_Index_Rate', 'Member_Months']].agg({'Metal':'first', 'AV':'first', 'Category':'first', 'Network':'first', 'Plan_Adjusted_Index_Rate':'first', 'Member_Months':'sum'})
-df_off_grouped = df_off.groupby('Plan_Name', as_index=False)[['Metal', 'AV', 'Category', 'Network', 'Plan_Adjusted_Index_Rate', 'Member_Months']].agg({'Metal':'first', 'AV':'first', 'Category':'first', 'Network':'first', 'Plan_Adjusted_Index_Rate':'first', 'Member_Months':'sum'})
-### onvert string to category ###
-# Convert Metal from string object to category
-df_on_grouped.Metal = df_on_grouped.Metal.astype('category')
-df_off_grouped.Metal = df_off_grouped.Metal.astype('category')
-# Convert "Category" column from string object to category
-df_on_grouped.Category = df_on_grouped.Category.astype('category')
-df_off_grouped.Category = df_off_grouped.Category.astype('category')
-# Convert "Network" column from string object to category
-df_on_grouped.Network = df_on_grouped.Network.astype('category')
-df_off_grouped.Network = df_off_grouped.Network.astype('category')
+df_on_grouped = df_on.groupby('Plan_Name', as_index=False)[['Metal', 'AV', 'Category', 'Network', 'Exchange', 'Plan_Adjusted_Index_Rate', 'Member_Months']].agg({'Metal':'first', 'AV':'first', 'Category':'first', 'Network':'first', 'Exchange':'first', 'Plan_Adjusted_Index_Rate':'first', 'Member_Months':'sum'})
+df_off_grouped = df_off.groupby('Plan_Name', as_index=False)[['Metal', 'AV', 'Category', 'Network', 'Exchange', 'Plan_Adjusted_Index_Rate', 'Member_Months']].agg({'Metal':'first', 'AV':'first', 'Category':'first', 'Network':'first', 'Exchange':'first', 'Plan_Adjusted_Index_Rate':'first', 'Member_Months':'sum'})
+# Convert "Exchange" column from string object to category
+df_on_grouped.Exchange = df_on_grouped.Exchange.astype('category')
+df_off_grouped.Exchange = df_off_grouped.Exchange.astype('category')
 ### SAVE ###
 # Save a .csv of this dataframe as backup
 df_on_grouped.to_csv(carrier + '_on.csv', index=False, encoding='utf8')
@@ -53,7 +49,20 @@ df_off_grouped.to_csv(carrier + '_off.csv', index=False, encoding='utf8')
 # Combine into single dataframe for plotting
 df = df_on_grouped.append(df_off_grouped, ignore_index=True)
 df.to_csv(carrier + '_cleaned.csv', index=False, encoding='utf8')
-
 ############
 ### PLOT ###
 ############
+style.use('fivethirtyeight')
+np_members = np.array(df.Member_Months * .05)
+col = df.Exchange.map({'Yes':'b', 'No':'r'})
+red_patch = mpatches.Patch(color='red', label='Off Exchange')
+blue_patch = mpatches.Patch(color='blue', label='On Exchange')
+# green_patch = mpatches.Patch(color='green', label='On Exchange (Select Network*)')
+# yellow_patch = mpatches.Patch(color='yellow', label='Off Exchange (Select Network*)')
+df.plot.scatter('AV', 'Plan_Adjusted_Index_Rate', s=np_members, marker='o', c=col, edgecolor='black')
+plt.xlabel('Actuarial Value (AV)')
+plt.ylabel('Plan Adjusted Index Rate')
+plt.title(carrier.title() + '\nVisualizing Plan Popularity (Marker Size)\nMeasured in Member Months')
+plt.grid(True)
+plt.legend(handles=[red_patch, blue_patch])
+plt.show()
